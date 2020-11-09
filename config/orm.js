46adb15deb5bf43c06a7ba83c0faf25f330f754a
Skyler R <skyler.rencher@gmail.com>
2020-11-08 20:32:29 -0500

    added the printQuestionMarks function to orm.js.

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+const connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
