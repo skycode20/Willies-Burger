48f093ba783ff47bdebac9225817363eadada2cb
Skyler R <skyler.rencher@gmail.com>
2020-11-08 20:12:35 -0500

    added some seeded entries in the seeds.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
 DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE IF NOT EXISTS burgers_db;
+
 USE burgers_db;
 
 DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
