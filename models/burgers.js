03ba2924df351922bc85890a3c1b50449eaa633d
Skyler R <skyler.rencher@gmail.com>
2020-11-08 20:55:21 -0500

    added the create and update methods to the burger const in burgers.js to call the specific orm functions.

--- models/burgers.js
+++ models/burgers.js
@@ -5,6 +5,19 @@ const burger = {
         orm.all("burgers", function (res) {
             cb(res);
         });
+    },
+    create: function (name, cb) {
+        orm.create("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function (id, cb) {
+        let condition = "id=" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, condition, cb);
     }
 };
 
