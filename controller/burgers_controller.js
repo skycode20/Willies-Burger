ff7d80b17f4223c15995dae5d81b1447983c947d
Skyler R <skyler.rencher@gmail.com>
2020-11-08 23:48:23 -0500

    added the post route.

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -7,11 +7,23 @@ router.get("/", function (req, res) {
     res.redirect("/burgers");
 });
 
+// get route
 router.get("/burgers", function (req, res) {
     // express callback response by calling burger.selectAllBurger
     burger.all(function (burgerData) {
         // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
         res.render("index", { burger_data: burgerData });
+    });
+});
+
+// post route -> back to index
+router.post("/burgers/create", function (req, res) {
+    // takes the request object using it as input for burger.addBurger
+    burger.create(req.body.burger_name, function (result) {
+        // wrapper for orm.js that using MySQL insert callback will return a log to console,
+        // render back to index with handle
+        console.log(result);
+        res.redirect("/");
     });
 });
 
